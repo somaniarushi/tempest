4d1b0facfd8719ecf5eb6a5d2204779502050df1
somaniarushi <arushisomani@berkeley.edu>
2020-07-18 17:29:17 -0700

    complete documentation

--- src/display.js
+++ src/display.js
@@ -18,6 +18,11 @@ class Display extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    /*
+    Handles a child note. It takes in a child list, the value of the child note,
+    and the tag of the child note and attaches it to the tree. Then, it sends
+    an alert to the app to update the main tree and refresh the display.
+    */
     handleSubmit(tree, value, currtag) {
         let note = {name: value, time: Date(), category: currtag, children: []}
         tree.push(note)
@@ -42,37 +47,15 @@ class Display extends React.Component {
     Displays the notes roots for the given project and tag.
     */
     displayLinks() {
-        const input = []
-        this.props.tree[this.state.project].forEach(note => {
-            if (this.state.tag === "all" || note.category === this.state.tag) {
-                const date = new Date(note.time)
-                const dateString = date.toLocaleString();
-                input.push(
-                    <div>
-                        <p className="date">{dateString}</p>
-                        <p className="text">{note.name}</p>
-                        <p className="category">{note.category}</p>
-                        <p className="project">{this.state.project}</p>
-                        <Submitter 
-                            submissionAdder={
-                                (value, currtag)=> {
-                                    this.handleSubmit(note.children, value, currtag)
-                                }
-                            }
-                            tags={this.props.list}
-                            projects={this.props.projects}
-                        />
-                        <div className="children">{this.displayChildren(note.children, this.state.project)}</div>
-                    </div>
-                );
-            }
-        });
+        const input = this.display(this.props.tree[this.state.project], this.state.project);
         return input
     }
 
-    displayChildren(children, project) {
+    /*
+    Recursively defined function that displays all the nodes in the tree with padding as required.
+    */
+    display(children, project) {
         const input = []
-        console.log("trying to display children")
         children.forEach(note => {
                 const date = new Date(note.time)
                 const dateString = date.toLocaleString();
@@ -92,7 +75,7 @@ class Display extends React.Component {
                             tags={this.props.list}
                             projects={this.props.projects}
                         />
-                        <div className="children">{this.displayChildren(note.children)}</div>
+                        <div className="children">{this.display(note.children)}</div>
                     </div>
                 );
             });
